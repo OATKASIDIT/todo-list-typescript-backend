f51199641ef5bac5116047979e43e7d74818e6c5
NatchaLatte <foxteary@gmail.com>
2024-11-22 17:21:33 +0700

    delete { force: true }

--- server.ts
+++ server.ts
@@ -33,7 +33,7 @@ app.use("/api", accountRoutes);
 const server = app.listen(port, async () => {
 	try{
 		await sequelize.authenticate();
-		await sequelize.sync({ force: true });
+		await sequelize.sync();
 		console.log("Connection has been established successfully.");
 	} catch (error) {
 		console.error("Unable to connect to the database:", error);
